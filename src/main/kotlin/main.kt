cda02ad130b1f0af205fd0446645cadc865ea50f
Shelq <klcao@yandex.ru>
2022-12-26 19:56:06 +0300

    Added HomeWork_1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
-fun main(){
-    println("Hello Kotlin")
+fun main() {
+    // Задание 1
+    val minAmount = 35
+    val amountTransfer = 20_000
+    val percentCommission = 0.75
+    val sumPercentCommission = (amountTransfer * percentCommission) / 100
+    val errorSumTransfer = "Сумма перевода меньше $minAmount руб."
+    var transfer = if (amountTransfer > minAmount) {
+        amountTransfer - sumPercentCommission
+    } else errorSumTransfer
+    println("Ваша сумма $amountTransfer руб.\nКомиссия $sumPercentCommission руб.\nСумма вашего перевода равна $transfer")
+
 }
