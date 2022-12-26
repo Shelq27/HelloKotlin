523851b867671b48fa00501821b3c155389088e2
Shelq <klcao@yandex.ru>
2022-12-26 20:35:07 +0300

    Added HomeWork_2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 fun main() {
-    // Задание 1
+// Задание 1
+    println("Задание 1")
     val minAmount = 35
     val amountTransfer = 20_000
     val percentCommission = 0.75
@@ -8,6 +9,19 @@ fun main() {
     var transfer = if (amountTransfer > minAmount) {
         amountTransfer - sumPercentCommission
     } else errorSumTransfer
-    println("Ваша сумма $amountTransfer руб.\nКомиссия $sumPercentCommission руб.\nСумма вашего перевода равна $transfer")
+    println("\tВаша сумма $amountTransfer руб.\n\tКомиссия $sumPercentCommission руб.\n\tСумма вашего перевода равна $transfer")
+
+// Задание 2
+    println("\nЗадание 2")
+    var likes = 61
+    var dependentText: String
+    if (likes % 10 == 1 && likes % 100 != 11) {
+        dependentText = "человеку"
+    } else dependentText = "людям"
+    println("\tПонравилось $likes $dependentText")
+
+// Задание 3
+    println("\nЗадание 3")
+    
 
 }
