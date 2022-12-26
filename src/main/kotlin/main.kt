679bfad0e4207066501dd7628a6508e7b23e6e8d
Shelq <klcao@yandex.ru>
2022-12-26 21:46:17 +0300

    Added scanner to HomeWork 1 and 2 , added HomeWork 3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,23 @@
+import java.util.Scanner
+
 fun main() {
+    val scanner = Scanner(System.`in`)
 // Задание 1
     println("Задание 1")
+    println("Введите сумму перевода ")
+    val amountTransfer = scanner.nextInt()
     val minAmount = 35
-    val amountTransfer = 20_000
     val percentCommission = 0.75
     val sumPercentCommission = (amountTransfer * percentCommission) / 100
     val errorSumTransfer = "Сумма перевода меньше $minAmount руб."
-    var transfer = if (amountTransfer > minAmount) {
-        amountTransfer - sumPercentCommission
-    } else errorSumTransfer
+    val transfer = if (amountTransfer > minAmount) {amountTransfer - sumPercentCommission} else errorSumTransfer
     println("\tВаша сумма $amountTransfer руб.\n\tКомиссия $sumPercentCommission руб.\n\tСумма вашего перевода равна $transfer")
 
 // Задание 2
     println("\nЗадание 2")
-    var likes = 61
-    var dependentText: String
+    println("Введите кол-во лайков ")
+    val likes = scanner.nextInt()
+    val dependentText: String
     if (likes % 10 == 1 && likes % 100 != 11) {
         dependentText = "человеку"
     } else dependentText = "людям"
@@ -22,6 +25,25 @@ fun main() {
 
 // Задание 3
     println("\nЗадание 3")
-    
-
+    println("Введите сумму покупки ")
+    val amount = scanner.nextInt()
+    val discountHundred = 100
+    val discountPercnet = .95
+    val discountRegular = .99
+    println("Введите regular если являетесь постоянным покупателем")
+    val inputRegular = scanner.next()
+    val regular: Boolean = inputRegular == "regular" || inputRegular == "Regular"
+    if (amount <= 1000) {
+        if (regular) {
+            println("Сумма вашей покупки равна " + amount * discountRegular)
+        } else println("Сумма вашей покупки равна $amount ")
+    } else if (amount > 1000 && amount <= 10_000) {
+        if (regular) {
+            println("Сумма вашей покупки равна " + (amount - discountHundred) * discountRegular)
+        } else println("Сумма вашей покупки равна " + (amount - discountHundred))
+    } else {
+        if (regular) {
+            println("Сумма вашей покупки равна " + (amount * discountPercnet) * discountRegular)
+        } else println("Сумма вашей покупки равна " + (amount * discountPercnet))
+    }
 }
